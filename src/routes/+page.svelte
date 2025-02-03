9e174dbfbe4f5881c2a715d87a1dac529c6c1826
jrgleason <jackiegleason@gmail.com>
2025-02-02 22:23:09 -0500

    Other stuff

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,3 +3,9 @@
     <h1>Welcome to SvelteKit</h1>
     <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 </div>
+<style>
+    @reference "tailwindcss/theme";
+    :global(html) {
+        background-color: theme(--color-gray-100);
+    }
+</style>
